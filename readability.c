3d8c670ea00f83fcae6bba2e5c488605a9d16a64
yatin-2999 <74901141+yatin-2999@users.noreply.github.com>
2020-12-01 01:07:41 +0530

    Update readability.c

--- readability.c
+++ readability.c
@@ -6,41 +6,41 @@
 
 int main(void)
 {
-  string s = get_string("text: ");
-  int letters = 0, words = 0, sentences = 0;
+    string s = get_string("text: ");
+    int letters = 0, words = 0, sentences = 0;
 
-  for(int i=0; i<strlen(s); i++)
-  {
-    if(isalpha(s[i]))
+    for (int i = 0; i < strlen(s); i++)
     {
-      letters++;
+        if (isalpha(s[i]))
+        {
+            letters++;
+        }
+        else if (isspace(s[i]))
+        {
+            words++;
+        }
+        else if (s[i] == '!' || s[i] == '?' || s[i] == '.')
+        {
+            sentences++;
+        }
     }
-    else if(isspace(s[i]))
+
+    words++;
+
+    float L = ((float)letters / (float)words) * 100;
+    float S = ((float)sentences / (float)words) * 100;
+    float grade = 0.0588 * L - 0.296 * S - 15.8;
+
+    if (grade < 16 && grade >= 0)
     {
-      words++;
+        printf("Grade %i\n", (int) round(grade));
     }
-    else if(s[i]=='!'||s[i]=='?'||s[i]=='.')
+    else if (grade >= 16)
     {
-      sentences++;
+        printf("Grade 16+\n");
     }
-  }
-
-  words++;
-
-  float L = ((float)letters / (float)words) * 100;
-  float S = ((float)sentences / (float)words) * 100;
-  float grade = 0.0588 * L - 0.296 * S - 15.8;
-
-  if (grade < 16 && grade >= 0)
-  {
-    printf("Grade %i\n", (int) round(grade));
-  }
-  else if (grade >= 16)
-  {
-    printf("Grade 16+\n");
-  }
-  else
-  {
-    printf("Before Grade 1\n");
-  }
+    else
+    {
+        printf("Before Grade 1\n");
+    }
 }
