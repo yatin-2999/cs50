2b66c20daedc7d7f33284a5854f49adb81a56db2
yatin-2999 <74901141+yatin-2999@users.noreply.github.com>
2020-12-01 00:22:28 +0530

    Create readability.c

---
+++ readability.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+  string text = get_string("text: ");
+  int letters = 0, words = 0, sentences = 0;
+  
+  for(int i=0; i<strlen(s); i++)
+  {
+    if(isalpha(s[i]))
+    {
+      letters++;
+    }
+    else if(isspace(s[i]))
+    {
+      words++;
+    }
+    else if(s[i]=='!'||s[i]=='?'||s[i]=='.')
+    {
+      sentences++;
+    }
+  }
+  
+  words++
+
+  float l = ((float)letters / (float)words) * 100;
+  float s = ((float)sentences / (float)words) * 100;
+  float grade = 0.0588 * l - 0.296 * s - 15.8;
+  
+  if (grade < 16 && grade >= 0)
+  {
+    printf("Grade %i\n", (int) round(grade));
+  }
+  else if (grade >= 16)
+  {
+    printf("Grade 16+\n");
+  }
+  else
+  {
+    printf("Before Grade 1\n");
+  }
+}
